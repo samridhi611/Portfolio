6a9dcf42f1b47974fcf56f16c515f2caec67b813
Samridhi <106435467+samridhi611@users.noreply.github.com>
2023-04-20 14:17:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function() {
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Developer","UI/UX Designer","Programmer","Software Developer"],
+        strings: ["Web Developer","UI/UX Designer","Programmer","Software Developer","Tech Enthusiast"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Web Developer","UI/UX Designer","Programmer","Software Developer" ],
+        strings: ["Web Developer","UI/UX Designer","Programmer","Software Developer","Tech Enthusiast"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
